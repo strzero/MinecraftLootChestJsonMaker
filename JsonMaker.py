538522e6f3c0831f63f589c21a6dfbb21e691781
Star0 <69407424+strzero@users.noreply.github.com>
2021-09-11 21:57:06 +0800

    Add files via upload

--- JsonMaker.py
+++ JsonMaker.py
@@ -1,5 +1,30 @@
+import os
 import re
+filedir =os.getcwd()
+filenames=os.listdir(filedir)
+f=open('results.json','w')
+f.writelines('{\n')
+first=True
+for filename in filenames:
+    if re.search('json', filename):
+        filepath = filedir+'/'+filename
+        if first is False :
+            f.writelines(',\n')
+        else:
+            first= False
+        f.writelines('"'+re.sub(r'\..*$', "", filename)+'": ')
+        for line in open(filepath):
+            f.writelines(line)
 
-print(re.search('(www)', 'www.runoob.com'))
-print ('aa')
-print (input())
+filedir =os.getcwd()+'/village'
+filenames=os.listdir(filedir)
+for filename in filenames:
+    if re.search('json', filename):
+        filepath = filedir+'/'+filename
+        f.writelines(',\n')
+        f.writelines('"'+re.sub(r'\..*$', "", filename)+'": ')
+        for line in open(filepath):
+            f.writelines(line)
+
+f.writelines('\n}')
+f.close()
